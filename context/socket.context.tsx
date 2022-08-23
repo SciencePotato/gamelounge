9d6276fd3dfff819c113603904f32eb9a306effd
SciencePotato <houchi6mail@gmail.com>
2022-08-22 23:03:32 -0400

    Created socket connection

--- context/socket.context.tsx
+++ context/socket.context.tsx
@@ -2,11 +2,13 @@ import { createContext, useContext } from "react";
 import { io } from "socket.io-client";
 import { SOCKET_URL } from "../config/default";
 
-const SocketContext = createContext({});
+const socket = io(SOCKET_URL);
+
+const SocketContext = createContext({socket});
 
 function SocketsProvider(props: any) {
     return (<SocketContext.Provider
-    value={{}}>
+    value={{socket}}>
     {...props}
     </SocketContext.Provider>)
 }
